eb7ba3d0abafe267b05c7804205d7a9fdcc65510
Nikola84ca <nicolateotorriatte@outlook.com>
2024-01-22 16:59:11 +0000

    Implemented the promptManager function to retrive manager data in index.js

--- index.js
+++ index.js
@@ -13,3 +13,45 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team = [];
+
+// This function will gather the information about the team manager
+
+function promptManager() {
+    inquirer
+      .prompt([
+       
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter your name:',
+              },
+              {
+                type: 'input',
+                name: 'id',
+                message: 'Enter your Employee ID:',
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'Enter your email address:',
+              },
+              {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Enter your office number:',
+              },
+        
+        ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        team.push(manager);
+        promptUser();
+      });
+  }
+  
